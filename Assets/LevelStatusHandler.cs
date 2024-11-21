9ca924e22b944d395fd43781b8f5f836488f282f
Your Name <tanboshen.tbs.ex@gmail.com>
2024-11-22 02:06:31 +0800

    daaaa

---
+++ Assets/LevelStatusHandler.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class LevelStatusHandler : EventHandler
+{
+    public Entity entity;
+    public TextMeshProUGUI LevelText;
+    public TextMeshProUGUI expStatus;
+    public Image expFill;
+    public override void OnInvoke(Trait ID, object data)
+    {
+        if (data is PlayableCharacterData)
+        {
+            entity = (PlayableCharacterData)data;
+            if (entity != null)
+            {
+                Unsubscribe(entity);
+            }
+            entity = (PlayableCharacterData)data;
+            Subscribe(entity);
+
+            expFill.fillAmount = entity.currentEXP / entity.ExpNeeded;
+            expStatus.text = string.Format("{0:0}%", (entity.currentEXP / entity.ExpNeeded) * 100);
+            LevelText.text = "Lv" + entity.lv;
+        }
+
+        if(data is float)
+        {
+            expFill.fillAmount = entity.currentEXP / entity.ExpNeeded;
+            expStatus.text = string.Format("{0:0}%", (entity.currentEXP / entity.ExpNeeded) * 100);
+            LevelText.text = "Lv" + entity.lv;
+        }
+    }
+
+}
